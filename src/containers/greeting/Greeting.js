df6e9f533a8acbde80d40286b03a89a1c7a4f1dd
kristveselii <kristveseli04@gmail.com>
2025-08-02 18:35:11 -0400

    8 - Fixed Resume Link

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./Krist Veseli Resume.pdf")}
+                    download="Krist Veseli Resume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
