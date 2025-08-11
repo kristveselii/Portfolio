6c778d29a21d77369270e7196a2d04326ba99e75
kristveselii <kristveseli04@gmail.com>
2025-08-11 19:10:41 -0400

    21 - Still not rendering

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./Krist Veseli Resume.pdf")}
-                    download="Krist Veseli Resume.pdf"
+                    href={require("./Editable Resume.docx")}
+                    download="Editable Resume.docx"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
