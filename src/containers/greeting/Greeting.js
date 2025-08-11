3257349101e9321661094fb3c194ee20acc6a991
kristveselii <kristveseli04@gmail.com>
2025-08-11 18:43:59 -0400

    18 - Resume Test

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./Krist Veseli Resume.pdf")}
-                    download="Krist Veseli Resume.pdf"
+                    href={require("./Krist Resume.docx")}
+                    download="Krist Resume.docx"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
